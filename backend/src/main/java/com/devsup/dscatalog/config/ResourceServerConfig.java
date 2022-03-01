e97be08797c1e6655d4d38e72fc732538b1b0dc2
Rommel Cysne <bcysne@yahoo.com.br>
2022-03-01 18:28:14 -0300

    feat: Test configuration - permit access to H2-console

--- backend/src/main/java/com/devsup/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsup/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsup.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -13,9 +16,15 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-	private static final String[] PUBLIC = { "/oauth/token" };
+	@Autowired
+	private Environment env;
+	
+	private static final String[] PUBLIC = { "/oauth/token",
+											 "/h2-console/**"};
+	
 	private static final String[] OPERATOR_OR_ADMIN = { "/products/**",
 														"/categories/**" };
+	
 	private static final String[] ADMIN = { "/users/**" };
 	
 	@Autowired
@@ -28,6 +37,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
 		http
 			.authorizeRequests()
 			.antMatchers(PUBLIC).permitAll()
