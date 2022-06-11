14d1c65ece5a01ee259a21791165fd5464314946
Rommel Cysne <bcysne@yahoo.com.br>
2022-06-11 11:53:20 -0300

    feat: Navbar styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from 'components/Navbar';
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-<h1>Hello DS Catalog</h1>
+    <>
+      <Navbar />
+      <h1>Hello DS Catalog</h1>
+    </>
   );
 }
 
