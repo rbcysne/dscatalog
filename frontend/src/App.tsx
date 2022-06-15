1a9290a810f7a3f45decf6ecd8839a961e504681
Rommel Cysne <bcysne@yahoo.com.br>
2022-06-14 21:59:28 -0300

    feat: New components
     - Product card
     - Product price

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Home from 'pages/Home';
 import './assets/styles/custom.scss';
 import './App.css';
+import Catalog from 'pages/Catalog';
 
 function App() {
   return (
     <>
-      <Home />
+      <Catalog />
     </>
   );
 }
---
+++ frontend/src/components/ProductPrice/index.tsx
@@ -0,0 +1,20 @@
+import './styles.css';
+
+const ProductPrice = () => {
+
+    return (
+        <div className="product-price-container">
+            <span>
+                R$
+            </span>
+
+            <h3>
+                2.733,15
+            </h3>
+
+        </div>
+
+    );
+}
+
+export default ProductPrice;
