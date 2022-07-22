d2008ecfe99e3551b680f840dda6f13745576ce1
Rommel Cysne <bcysne@yahoo.com.br>
2022-07-21 23:05:19 -0300

    feat: Links
     - Products catalog <-> Product details

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,53 +1,64 @@
 import ProductCard from 'components/ProductCard';
+import { Link } from 'react-router-dom';
 import { Product } from 'types/products';
 
 const Catalog = () => {
-
-    const prod : Product = {
-        "id": 2,
-        "name": "Smart TV",
-        "description": "Uma TV",
-        "price": 2193.0,
-        "imgUrl": "https://m.media-amazon.com/images/I/81IdScEcX2S._AC_SX679_.jpg",
-        "date": "2020-07-14T10:00:00Z",
-        "categories": [
+    const prod: Product = {
+        id: 2,
+        name: 'Smart TV',
+        description: 'Uma TV',
+        price: 2193.0,
+        imgUrl: 'https://m.media-amazon.com/images/I/81IdScEcX2S._AC_SX679_.jpg',
+        date: '2020-07-14T10:00:00Z',
+        categories: [
             {
-                "id": 1,
-                "name": "Livros"
+                id: 1,
+                name: 'Livros',
             },
             {
-                "id": 3,
-                "name": "Computadores"
-            }
-        ]
-
-    }
+                id: 3,
+                name: 'Computadores',
+            },
+        ],
+    };
     return (
         <>
             <div className="container my-4">
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard product={prod}/>
+                        <Link to="catalog/1">
+                            <ProductCard product={prod} />
+                        </Link>
                     </div>
 
                     <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard product={prod}/>
+                        <Link to="catalog/1">
+                            <ProductCard product={prod} />
+                        </Link>
                     </div>
 
                     <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard product={prod}/>
+                        <Link to="catalog/1">
+                            <ProductCard product={prod} />
+                        </Link>
                     </div>
 
                     <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard product={prod}/>
+                        <Link to="catalog/1">
+                            <ProductCard product={prod} />
+                        </Link>
                     </div>
 
                     <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard product={prod}/>
+                        <Link to="catalog/1">
+                            <ProductCard product={prod} />
+                        </Link>
                     </div>
 
                     <div className="col-sm-6 col-lg-4 col-xl-3">
-                        <ProductCard product={prod}/>
+                        <Link to="catalog/1">
+                            <ProductCard product={prod} />
+                        </Link>
                     </div>
                 </div>
             </div>
--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,37 +1,45 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
 const ProductDetails = () => {
-
     return (
         <div className="product-details-container">
             <div className="base-card product-details-card">
-                <div className="back-container">
-                    <ArrowIcon /> <h2>Voltar</h2>
-                </div>
+                <Link to="/catalog">
+                    <div className="back-container">
+                        <ArrowIcon /> <h2>Voltar</h2>
+                    </div>
+                </Link>
                 <div className="row">
                     <div className="col-xl-6">
                         <div className="img-container">
-                            <img src="https://m.media-amazon.com/images/I/81IdScEcX2S._AC_SX679_.jpg" alt="Nome do Produto" />
+                            <img
+                                src="https://m.media-amazon.com/images/I/81IdScEcX2S._AC_SX679_.jpg"
+                                alt="Nome do Produto"
+                            />
                         </div>
                         <div className="name-price-container">
                             <h1>Nome do produto</h1>
-                            <ProductPrice price={2962.23}/>
+                            <ProductPrice price={2962.23} />
                         </div>
                     </div>
 
                     <div className="col-xl-6">
                         <div className="description-container">
                             <h2>Descrição do Produto</h2>
-                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta, eaque!</p>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur,
+                                adipisicing elit. Soluta, eaque!
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default ProductDetails;
