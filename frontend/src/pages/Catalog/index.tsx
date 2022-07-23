289f2cb7967a5de77b49ae9ed3d15b8479ee7fc5
Rommel Cysne <bcysne@yahoo.com.br>
2022-07-23 14:11:04 -0300

    feat: New component
     - Pagination
     - styling

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,6 +1,9 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/products';
+
+import './styles.css';
 
 const Catalog = () => {
     const prod: Product = {
@@ -23,7 +26,11 @@ const Catalog = () => {
     };
     return (
         <>
-            <div className="container my-4">
+            <div className="container my-4 catalog-container">
+                <div className="row catalog-title-container">
+                    <h1>Catálogo de produtos</h1>
+                </div>
+
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3">
                         <Link to="catalog/1">
@@ -60,6 +67,10 @@ const Catalog = () => {
                             <ProductCard product={prod} />
                         </Link>
                     </div>
+                </div>
+
+                <div className="row">
+                    <Pagination />
                 </div>
             </div>
         </>
