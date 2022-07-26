a3af783515edd8d5a5b8f90d5af20e493b562bed
Rommel Cysne <bcysne@yahoo.com.br>
2022-07-25 22:21:49 -0300

    feat: Product details loaders
     - ProductDetailsLoader
     - ProductImgLoader

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -5,24 +5,31 @@ import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { Product } from 'types/product';
 import { BASE_URL } from 'util/requests';
+import ProductDetailsLoader from './ProductDetailsLoader';
+import ProductImgLoader from './ProductImgLoader';
 
 import './styles.css';
 
 type UrlParams = {
     productId: string;
-}
+};
 
 const ProductDetails = () => {
-
     const { productId } = useParams<UrlParams>();
 
     const [product, setProduct] = useState<Product>();
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/products/${productId}`)
-        .then(response => {
-            setProduct(response.data);
-        });
+        setIsLoading(true);
+        axios
+            .get(`${BASE_URL}/products/${productId}`)
+            .then((response) => {
+                setProduct(response.data);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
     }, [productId]);
 
     return (
@@ -35,25 +42,31 @@ const ProductDetails = () => {
                 </Link>
                 <div className="row">
                     <div className="col-xl-6">
-                        <div className="img-container">
-                            <img
-                                src={product?.imgUrl}
-                                alt={product?.name}
-                            />
-                        </div>
-                        <div className="name-price-container">
-                            <h1>{product?.name}</h1>
-                            {product && <ProductPrice price={product?.price} />}
-                        </div>
+                        { isLoading ? <ProductImgLoader /> :
+                            <>
+                                <div className="img-container">
+                                    <img
+                                        src={product?.imgUrl}
+                                        alt={product?.name}
+                                    />
+                                </div>
+                                <div className="name-price-container">
+                                    <h1>{product?.name}</h1>
+                                    {product && (
+                                        <ProductPrice price={product?.price} />
+                                    )}
+                                </div>
+                            </>
+                        }
                     </div>
 
                     <div className="col-xl-6">
-                        <div className="description-container">
-                            <h2>Descrição do Produto</h2>
-                            <p>
-                                {product?.description}
-                            </p>
-                        </div>
+                        { isLoading ? <ProductDetailsLoader /> :
+                            <div className="description-container">
+                                <h2>Descrição do Produto</h2>
+                                <p>{product?.description}</p>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
