3d3afb16ae1eff4e13a7c3f98961d7a732ae5d7f
Rommel Cysne <bcysne@yahoo.com.br>
2022-12-04 21:11:41 -0300

    feat: Form validation with React Hook Form
     - required field validation
     - pattern validation

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -12,7 +12,7 @@ const Login = () => {
 
     const [hasError, setHasError] = useState(false);
 
-    const {register, handleSubmit} = useForm<FormData>();
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
 
     const onSubmit = (formData : FormData) => {
         requestBackendLogin(formData)
@@ -39,14 +39,32 @@ const Login = () => {
 
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
-                    <input {...register("username")} type="text" 
+                    <input 
+                        {...register("username", {
+                                required: 'Campo obrigatório',
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    message: 'Email inválido',
+                                }
+                            })
+                        } 
+                        type="text" 
                         className="form-control base-input"
-                        placeholder="Email" name="username" />
+                        placeholder="Email" 
+                        name="username" />
+                    <div className="invalid-feedback d-block">{errors.username?.message}</div>
                 </div>
                 <div className="mb-2">
-                    <input {...register("password")} type="password" 
+                    <input 
+                        {...register("password", {
+                                required: 'Campo obrigatório'
+                            })
+                        } 
+                        type="password" 
                         className="form-control base-input " 
-                        placeholder="Password" name="password" />
+                        placeholder="Password" 
+                        name="password" />
+                    <div className="invalid-feedback d-block">{errors.password?.message}</div>
                 </div>
                     <Link to="/admin/auth/recover" className="login-link-recover">
                         Esqueci a senha
