c035388565f87811be2eb3254c01975c9e6ba8df
Rommel Cysne <bcysne@yahoo.com.br>
2023-06-17 21:33:38 -0300

    feat: Form styling
        - buttons and inputs styling

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -7,25 +7,47 @@ const Form = () => {
                 <h1 className="product-form-card-title">Dados do Produto</h1>
 
                 <form action="">
-                    <div className="row">
-                        <div className="col-lg-6">
-                            <input 
-                                type="text" 
-                                className="form-control base-input" 
-                                placeholder="Nome do Produto" />
-                            <input type="text" className="form-control base-input" />
-                            <input type="text" className="form-control base-input" />
-                        </div>
-                        <div className="col-lg-6">
-                            <textarea name=""rows={10} className="form-control base-input"></textarea>
+                    <div className="row product-form-card-info-container">
+                        <div className="col-lg-6 product-form-card-details">
+                            <div className="margin-bottom-30">
+                                <input 
+                                    type="text" 
+                                    className="form-control base-input" 
+                                    placeholder="Nome do Produto" />
+                            </div>
+
+                            <div className="margin-bottom-30">
+                                <input 
+                                    type="text" 
+                                    className="form-control base-input"
+                                    placeholder="Categoria" />
+                            </div>
+
+                            <div className="">
+                                <input 
+                                    type="text" 
+                                    className="form-control base-input"
+                                    placeholder="Preço" />
+                            </div>
 
                         </div>
+                        <div className="col-lg-6 product-form-card-description">
+                            <div>
+                                <textarea 
+                                    name="" 
+                                    rows={10} 
+                                    className="form-control base-input h-auto"
+                                    placeholder="Descrição">
+                                </textarea>
+                            </div>
+                        </div>
                     </div>
-                    <div >
-                        <button className="btn btn-outline-danger">
+
+                    <div className="product-form-card-buttons-container">
+                        <button className="btn btn-outline-danger btn-form-card">
                             Cancelar
                         </button>
-                        <button className="btn btn-primary">
+                        <button className="btn btn-primary btn-form-card">
                             Salvar
                         </button>
 
